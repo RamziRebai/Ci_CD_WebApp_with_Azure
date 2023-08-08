727188f28d5a47ce4d47a1a3b4ebeb4e97dc5903
RamziRebai <92611463+RamziRebai@users.noreply.github.com>
2023-08-08 16:36:21 +0000

    upload my app

---
+++ Makefile
@@ -0,0 +1,22 @@
+install:
+		pip install --upgrade pip &&\
+			pip install -r requirements.txt
+test:
+		python -m pytest -vv -cov=tests tests.py 
+lint:
+		pylint --disable=R,C app/main.py
+format:
+		black *.py
+run:	
+		python app/main.py
+run-uvicorn:
+		uvicorn app.main:app --port 8000 --reload
+killweb:
+		sudo killall uvicorn
+container-lint:
+		docker run --rm -i hadolint/hadolint < Dockerfile
+distroless-lint:
+		docker run --rm -i hadolint/hadolint < Distroless.Dockerfile
+
+all:	
+		install lint pytest run-uvicorn
---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+from pydantic import BaseModel
+
+from transformers import pipeline
+
+app=FastAPI()
+
+@app.on_event("startup")
+def load_model():
+    global generator
+    generator= pipeline('text-generation', model='gpt2')
+
+class Inputss(BaseModel):
+    text: str 
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+        <body>
+            <h1> Everything is going well, please add '/docs' at the end to interact with the model
+            </h1>
+        </body>
+    </html>
+    """
+
+@app.post("/generate")
+def predict(phrase:Inputss):
+    result= generator(phrase.text, min_length=10, max_length=100, num_return_sequences=1)
+    return result[0]["generated_text"]
+
+if __name__=='__main__':
+    uvicorn.run(app, host="0.0.0.0", port="8000")
+
+
+
